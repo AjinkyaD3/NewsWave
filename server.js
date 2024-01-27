3f25ebb74022fd946817b559ba13e21cb0460f0e
Ajinkya <ajinkyadhotre201@gmail.com>
2024-01-27 08:45:13 +0530

    Creating ui

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send("hello World!")
+    res.render("index")
 })
 
 
