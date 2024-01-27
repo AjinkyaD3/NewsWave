f2d07832dfeb68ad48d37536088c1640e5a27d05
Ajinkya <ajinkyadhotre201@gmail.com>
2024-01-27 12:05:17 +0530

    Working now

--- server.js
+++ server.js
@@ -1,24 +1,42 @@
 require('dotenv').config();
-const express = require('express')
-const bodyParser = require("body-parser");
-const app = express()
-const path = require('path')
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const path = require('path');
 const port = 3000;
 
+const NewsAPI = require('newsapi');
+const apiKey = process.env.API_KEY;
+const newsapi = new NewsAPI(apiKey);
 
-app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.render("index")
-})
+app.get('/', async (req, res) => {
+    const userSearchString = req.query.q || 'technology';
+    const userPageNumber = parseInt(req.query.page) || 1;
+    const lang = req.query.lang || 'en';
+    let articles = []
 
+    try {
+        articles = await newsapi.v2.everything({
+            q: userSearchString,
+            page: userPageNumber,
+            language: lang,
+        });
 
-
-
-
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+    // console.log(articles)
+    // console.log(typeof articles)
+    // res.json(articles)
+    res.render('index', { article: articles });
+});
 
 app.listen(port, () => {
-    console.log("Server Started On http://localhost:3000")
-})
+    console.log('Server Started On http://localhost:3000');
+});
